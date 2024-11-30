a569f5d99826d7d502deed7d10eec3b17936786e
nbilbo <ddtankx68@gmail.com>
2024-11-30 15:01:56 -0300

    Create src/i18n/languages/pt_BR.ts

---
+++ src/i18n/languages/pt_BR.ts
@@ -0,0 +1,38 @@
+import Key from '../i18nKey'
+import type { Translation } from '../translation'
+
+export const pt_BR: Translation = {
+  [Key.home]: 'Inicio',
+  [Key.about]: 'Sobre',
+  [Key.archive]: 'Arquivos',
+  [Key.search]: 'Pesquisar',
+
+  [Key.tags]: 'Etiquetas',
+  [Key.categories]: 'Categorias',
+  [Key.recentPosts]: 'Postagens recentes',
+
+  [Key.comments]: 'Comentários',
+
+  [Key.untitled]: 'Sem título',
+  [Key.uncategorized]: 'Sem categoria',
+  [Key.noTags]: 'Sem etiquetas',
+
+  [Key.wordCount]: 'palavra',
+  [Key.wordsCount]: 'palavras',
+  [Key.minuteCount]: 'minuto',
+  [Key.minutesCount]: 'minutos',
+  [Key.postCount]: 'postagem',
+  [Key.postsCount]: 'postagens',
+
+  [Key.themeColor]: 'Cor do tema',
+
+  [Key.lightMode]: 'Claro',
+  [Key.darkMode]: 'Escuro',
+  [Key.systemMode]: 'Sistema',
+
+  [Key.more]: 'Mais',
+
+  [Key.author]: 'Autor',
+  [Key.publishedAt]: 'Publicado em',
+  [Key.license]: 'Licença',
+}
